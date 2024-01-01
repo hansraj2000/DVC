f0885cf6309a155f1b9609a45baeff1f38b40a99
hansraj2000 <hansrajkr2120@gmail.com>
2024-01-01 15:37:54 +0530

    test.py added

---
+++ test.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+data=[
+    {"name":"Hans","age":23,"city":"New Delhi"},
+    {"name":"Mohit","age":23,"city":"New Delhi"},
+    {"name":"Yash","age":23,"city":"New Delhi"},
+]
+
+df = pd.DataFrame(data)
+
+df.to_csv("data/data.csv", index=False)
